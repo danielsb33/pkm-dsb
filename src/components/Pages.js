c55473114d196e57928995bcbfca9f47f3d99bc7
Danisb <danisanchezbravo@gmail.com>
2022-03-18 09:11:02 +0100

    Unicode v2

--- src/components/Pages.js
+++ src/components/Pages.js
@@ -4,8 +4,8 @@ const Pages = ({ pokemonData, setOffset }) => {
 
     return (
         <div className = "PagesBtns">
-            <button onClick = {() => pokemonData.previous ? setOffset(pokemonData.previous.slice(pokemonData.previous.indexOf("?"))) : null}>&#129052;</button>{" "}
-            <button onClick = {() => pokemonData.next ? setOffset(pokemonData.next.slice(pokemonData.next.indexOf("?"))) : null}>&#129054;</button>{" "}
+            <button onClick = {() => pokemonData.previous ? setOffset(pokemonData.previous.slice(pokemonData.previous.indexOf("?"))) : null}>&#5130;</button>{" "}
+            <button onClick = {() => pokemonData.next ? setOffset(pokemonData.next.slice(pokemonData.next.indexOf("?"))) : null}>&#5125;</button>{" "}
         </div>
     );
 };
