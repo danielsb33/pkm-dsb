30860a492e53676c2a46ba1480d2bbc83621b749
Danisb <danisanchezbravo@gmail.com>
2022-03-18 03:46:40 +0100

    fix unicode symbols

--- src/components/Pages.js
+++ src/components/Pages.js
@@ -4,8 +4,8 @@ const Pages = ({ pokemonData, setOffset }) => {
 
     return (
         <div className = "PagesBtns">
-            <button onClick = {() => pokemonData.previous ? setOffset(pokemonData.previous.slice(pokemonData.previous.indexOf("?"))) : null}>🠜</button>{" "}
-            <button onClick = {() => pokemonData.next ? setOffset(pokemonData.next.slice(pokemonData.next.indexOf("?"))) : null}>🠞</button>{" "}
+            <button onClick = {() => pokemonData.previous ? setOffset(pokemonData.previous.slice(pokemonData.previous.indexOf("?"))) : null}>&#129052;</button>{" "}
+            <button onClick = {() => pokemonData.next ? setOffset(pokemonData.next.slice(pokemonData.next.indexOf("?"))) : null}>&#129054;</button>{" "}
         </div>
     );
 };
